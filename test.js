5530feb9c13811195a23fb86ba00d9c3164d4736
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-01-08 22:57:35 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+//  Variable summary Here we see how we can use variables in a javascript
+
+const accountID = 14432
+let accountEmail = "ankush@gmail.com"
+var accountpsswd = "212121"
+accountype = "321"
+let accountstate;
+
+//  accountID = 2 not allowed to change values in constant variable 
+
+ accountEmail = "ankush@gmail.com"
+ accountpsswd = "212121"
+ accountype = "321"
+
+ console.log(accountID);
+ console.table([accountID , accountEmail , accountpsswd , accountype , accountstate]);
+
+//  phle ke time me var use hota th pr ab ni bcz due to block scope and functional scope ab do hi variable jyada tr use m aaata hn let or const
+
+// ek sth show krna ho multiple variable names ko to table k use krte h 
+
+
+
+ 
