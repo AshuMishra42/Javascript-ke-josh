9719c049693198dc458b385464b14eab4f1782db
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-01-10 19:40:36 +0530

    Add files via upload

---
+++ operations.js
@@ -0,0 +1,33 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// Basic operations:-
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**5);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+// For  Boolean output:-
+
+console.log(+true);
+
+// Normal output:- 
+
+let str1= "Ankush"
+let str2= " Mishra"
+let str3= str1+str2
+console.log(str3);
+
+let gamecounter= 100
+gamecounter++;
+console.log(gamecounter);
+
