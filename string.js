ae69e622da79e121ced3992e68f015314d19b23a
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-01-12 22:32:13 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,16 @@
+const naam ="Ankush"
+const repocount = 5
+
+console.log(naam + repocount + "value" );
+
+// Previously we used this type of technique to con-catenate  to two or more variables
+
+console.log(`Hello my name is ${naam} and my repocount is ${repocount}`);
+
+// Here is the new trick to con-catenate two variables. 
+// we can declare string in another type
+
+const gameName = new String("Anshu");
+console.log(gameName);
+
+//  Here by using keyword new we are using object
