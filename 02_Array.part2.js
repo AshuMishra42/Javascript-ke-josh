8850639b092b24efca311c648a3a0652bda8c249
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-01-18 23:59:43 +0530

    Add files via upload

---
+++ 02_Array.part2.js
@@ -0,0 +1,14 @@
+let marverl_heroes=["thor","spiderman","bheem","Ironman"]
+let dc_heroes=["Ankush", "satyam","vikash","saurav"]
+
+marverl_heroes.push(dc_heroes)
+console.log(marverl_heroes);
+
+// Array ke andr array aa j rha hn hmlog soche th ki dono array add hoga but aesa hua ni hn array koi bhi data le leta hn 
+// yha p v aise hi le liya input array ko array ek single element smjh k input le liya hn 
+
+//particular value ko find krna ho through indeing to aese krege 
+
+console.log(marverl_heroes[4][3]);
+
+// Then it easily gives the answer value = saurav 
