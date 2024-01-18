6e45ecc2b3f8a82b6087f633a6409a90d7088955
Ankush Mishra <144258860+AshuMishra42@users.noreply.github.com>
2024-01-19 00:24:03 +0530

    Update 02_Array.part2.js

--- 02_Array.part2.js
+++ 02_Array.part2.js
@@ -12,3 +12,35 @@ console.log(marverl_heroes);
 console.log(marverl_heroes[4][3]);
 
 // Then it easily gives the answer value = saurav 
+
+// by using concatenation we can able to add two array easily 
+
+let new_heroes= marverl_heroes.concat(dc_heroes)
+console.log(new_heroes)
+
+// by help of concatenation we are able to add two arrays but currently mostly used spread operator:-
+
+let all_heroes=[...marverl_heroes,...dc_heroes]
+console.log(all_heroes);
+
+// it also adds two or more than two given array 
+
+// agr aes rhe to array ke andr or hm chhahte hn sare elemnts ko normally ek array me dekhna to aese karege 
+
+const another_array=[1,2,3,4[5,6,7],8,9,10,[11,12,13,],14,15[16]]
+const real_array=another_array.flat(Infinity)
+console.log(real_array);
+
+//Questioning array :-
+
+console.log(Array.isArray("ankush")); // answer gives false because it is in the form of string 
+
+console.log(Array.from("Ankush")); // now it is easily converted into array
+
+console.log(Array.from({name:"ankush"})); // it gives [] blank array because pta hi ni chl rha hn kis key pair ko array bnayege 
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));  // output shown be like this [100,200,300] of() method returns a new array from  a set of elements 
